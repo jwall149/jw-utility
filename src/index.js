8025bdf14f9f941570722441a9faadadc79385cc
Tonny Pham <jwall149@gmail.com>
2016-07-11 15:55:07 +0900

    Expose to default

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './utils/date';
+import './utils/string';
+import Settings from './settings';
+import Regexs from './cons/regexps';
+import _ from './utils/underscore';
+import ValidatorBase from './validators/base';
+import FieldBase from './validators/fields/base';
+
+export { _, Settings, ValidatorBase, FieldBase };
